186fcaa5c4ffbe93af3fac362342e7a00fc3ec70
Nicolas Leonardo Miranda Lima <nicolas.lima@in.cassems.com.br>
2022-09-20 16:03:37 -0400

    websocket front-end for tests

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Panel from '../views/Panel.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Panel',
+    component: Panel
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/Caller',
+    name: 'Caller',
+    component: () => import('../views/Caller.vue')
   }
 ]
 
---
+++ src/views/Panel.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <div class="panel">
+      <div class="password">
+        {{password}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { io } from 'socket.io-client';
+
+export default {
+  data() {
+    return {
+      password: ''
+    }
+  },
+  created() {
+    const socket = io('http://localhost:3000');
+    
+    socket.on('connection', (data) => console.log(data));
+    socket.on('attendanceCall', (data) => {
+      console.log(data.password)
+      this.password = data.password
+    });
+
+  }
+}
+
+</script>
+
+<style scoped>
+
+* {
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.panel {
+  height: 150px;
+  width: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(224, 215, 215);
+  
+}
+</style>
